5b2e064135fc606fc5f772348975c5c33b00881c
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-12 19:11:17 +0200

    test(models): add invalid amount test for consignar

--- src/test/java/dev/jenny/bankaccount/models/CuentaTest.java
+++ src/test/java/dev/jenny/bankaccount/models/CuentaTest.java
@@ -3,9 +3,12 @@ package dev.jenny.bankaccount.models;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CuentaTest {
 
@@ -30,5 +33,13 @@ public class CuentaTest {
         cuenta.consignar(500f);
         assertThat(cuenta.getSaldo(), is(equalTo(15500f)));
         assertThat(cuenta.getNumeroConsignaciones(), is(equalTo(1)));
+    }
+
+    @ParameterizedTest(name = "consignar({0}) should throw exception")
+    @ValueSource(floats = { 0f, -100f })
+    void testConsignar_InvalidAmount_ShouldThrowException(float cantidad) {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> cuenta.consignar(cantidad));
+        assertThat(exception.getMessage(), is(equalTo("La cantidad debe ser mayor que cero")));
     }
 }
