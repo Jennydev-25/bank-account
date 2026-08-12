8c9bb6c50ddd8cc384aa2218599875e08f8ff9b3
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-12 19:13:39 +0200

    feat(models): validate amount in consignar

--- src/main/java/dev/jenny/bankaccount/models/Cuenta.java
+++ src/main/java/dev/jenny/bankaccount/models/Cuenta.java
@@ -45,6 +45,9 @@ public class Cuenta {
     }
 
     public void consignar(float cantidad) {
+        if (cantidad <= 0) {
+            throw new IllegalArgumentException("La cantidad debe ser mayor que cero");
+        }
         saldo += cantidad;
         numeroConsignaciones++;
     }
