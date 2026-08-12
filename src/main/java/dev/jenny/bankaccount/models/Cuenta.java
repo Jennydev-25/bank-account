4cd64d3865bfc841d802dc29974b57321275f6b1
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-12 18:57:43 +0200

    feat(models): add Cuenta constructor and getters

---
+++ src/main/java/dev/jenny/bankaccount/models/Cuenta.java
@@ -0,0 +1,46 @@
+package dev.jenny.bankaccount.models;
+
+/**
+ * Represents a generic bank account with the operations shared by every account
+ * type
+ */
+public class Cuenta {
+
+    protected float saldo;
+    protected int numeroConsignaciones;
+    protected int numeroRetiros;
+    protected float tasaAnual;
+    protected float comisionMensual;
+
+    /**
+     * Creates an account with the given balance and annual interest rate as a
+     * percentage (3 means 3%)
+     */
+    public Cuenta(float saldo, float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+        this.numeroConsignaciones = 0;
+        this.numeroRetiros = 0;
+        this.comisionMensual = 0f;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public int getNumeroConsignaciones() {
+        return numeroConsignaciones;
+    }
+
+    public int getNumeroRetiros() {
+        return numeroRetiros;
+    }
+
+    public float getTasaAnual() {
+        return tasaAnual;
+    }
+
+    public float getComisionMensual() {
+        return comisionMensual;
+    }
+}
