562bd079482eb6649190958f8b2a7a6af2f8237d
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-13 11:51:47 +0200

    refactor(models): extract validateActive in SavingsAccount

--- src/main/java/dev/jenny/bankaccount/models/SavingsAccount.java
+++ src/main/java/dev/jenny/bankaccount/models/SavingsAccount.java
@@ -20,21 +20,23 @@ public class SavingsAccount extends Account {
 
     @Override
     public void deposit(float amount) {
-        if (!active) {
-            throw new IllegalStateException("Account is inactive");
-        }
+        validateActive();
         super.deposit(amount);
     }
 
     @Override
     public void withdraw(float amount) {
-        if (!active) {
-            throw new IllegalStateException("Account is inactive");
-        }
+        validateActive();
         super.withdraw(amount);
     }
 
     private void updateActiveStatus() {
         active = balance >= 10000;
+    }
+
+    private void validateActive() {
+        if (!active) {
+            throw new IllegalStateException("Account is inactive");
+        }
     }
 }
