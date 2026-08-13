4f90400f8f0871fa62111940d797dd0d38b02d1a
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-13 14:21:46 +0200

    test(models): add withdraw test for CheckingAccount within balance

--- src/test/java/dev/jenny/bankaccount/models/CheckingAccountTest.java
+++ src/test/java/dev/jenny/bankaccount/models/CheckingAccountTest.java
@@ -4,7 +4,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CheckingAccountTest {
 
@@ -14,5 +19,22 @@ public class CheckingAccountTest {
         assertThat(checkingAccount.getBalance(), is(equalTo(15000f)));
         assertThat(checkingAccount.getAnnualRate(), is(equalTo(3f)));
         assertThat(checkingAccount.getOverdraft(), is(equalTo(0f)));
+    }
+
+    @ParameterizedTest(name = "withdrawing {0} from a balance of 15000 should leave balance {1} without overdraft")
+    @MethodSource("withdrawWithinBalanceTestCases")
+    void testWithdraw_AmountWithinBalance_ShouldUpdateBalanceOnly(float amount, float expectedBalance) {
+        CheckingAccount checkingAccount = new CheckingAccount(15000f, 3f);
+
+        checkingAccount.withdraw(amount);
+
+        assertThat(checkingAccount.getBalance(), is(equalTo(expectedBalance)));
+        assertThat(checkingAccount.getOverdraft(), is(equalTo(0f)));
+    }
+
+    private static Stream<Arguments> withdrawWithinBalanceTestCases() {
+        return Stream.of(
+                Arguments.of(5000f, 10000f),
+                Arguments.of(15000f, 0f));
     }
 }
