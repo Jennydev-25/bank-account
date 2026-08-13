a7cec4ce9e38b08b521f348422d2834c10fbd642
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-13 13:28:52 +0200

    feat(models): add CheckingAccount constructor with overdraft

---
+++ src/main/java/dev/jenny/bankaccount/models/CheckingAccount.java
@@ -0,0 +1,19 @@
+package dev.jenny.bankaccount.models;
+
+/**
+ * Represents a checking account: withdrawals can exceed the balance, the
+ * difference is tracked as overdraft
+ */
+public class CheckingAccount extends Account {
+
+    protected float overdraft;
+
+    public CheckingAccount(float balance, float annualRate) {
+        super(balance, annualRate);
+        this.overdraft = 0f;
+    }
+
+    public float getOverdraft() {
+        return overdraft;
+    }
+}
