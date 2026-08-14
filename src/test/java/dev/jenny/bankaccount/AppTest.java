77e301bd8b3649d61d0e54fb54cb25ddd9717cd0
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-14 22:27:42 +0200

    test(app): add createDemoAccounts test for App

--- src/test/java/dev/jenny/bankaccount/AppTest.java
+++ src/test/java/dev/jenny/bankaccount/AppTest.java
@@ -1,18 +1,36 @@
 package dev.jenny.bankaccount;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+
+import dev.jenny.bankaccount.models.Account;
+import dev.jenny.bankaccount.models.CheckingAccount;
+import dev.jenny.bankaccount.models.SavingsAccount;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-/**
- * Unit test for simple App.
- */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testCreateDemoAccounts_ShouldReturnThreeAccountsWithExpectedState() {
+        List<Account> accounts = App.createDemoAccounts();
+
+        assertThat(accounts, hasSize(3));
+
+        Account account = accounts.get(0);
+        assertThat((double) account.getBalance(), is(closeTo(15338.25, 0.01)));
+
+        SavingsAccount savingsAccount = (SavingsAccount) accounts.get(1);
+        assertThat((double) savingsAccount.getBalance(), is(closeTo(12731.75, 0.01)));
+        assertThat(savingsAccount.isActive(), is(true));
+
+        CheckingAccount checkingAccount = (CheckingAccount) accounts.get(2);
+        assertThat((double) checkingAccount.getBalance(), is(closeTo(2005.0, 0.01)));
+        assertThat(checkingAccount.getOverdraft(), is(equalTo(3000f)));
     }
 }
